49f9fb22b63c7d9d7fb15f0dc68b8b9bd0e26d0a
Zak Strassberg <zakstrassberg@gmail.com>
2018-08-25 00:02:40 -0400

    add to types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,15 +1,125 @@
+import firebase from 'firebase';
+
 export interface Combatant {
-    name: string;
-    initiative: number;
-    turnOver: boolean;
-    evasion: number;
-    parry: number;
-    onslaught: number;
-    willpower: number;
-    woundPenalty: number;
+  name: string;
+  initiative: number;
+  turnOver: boolean;
+  evasion: number;
+  parry: number;
+  onslaught: number;
+  willpower: number;
+  woundPenalty: number;
+  character?: firebase.firestore.DocumentReference;
 }
 
 export interface Message {
-    createdAt: Date;
-    body: string;
+  createdAt: Date;
+  body: string;
 }
+
+export interface Character {
+  name: string;
+  attributes: { [A in Attribute]: number };
+  abilities: { [A in Ability]: number };
+  specialties: { [A in Ability]?: string };
+  merits: {
+    [meritName: string]: number;
+  };
+  willpower: {
+    permanent: number;
+    temporary: number;
+  };
+  limit: number;
+  limitTrigger: string;
+  essence: number;
+  motes: {
+    personalTemporary: number;
+    peripheralTemporary: number;
+    personalPermanent: number;
+    peripheralPermanent: number;
+  };
+  experience: {
+    regularCurrent: number;
+    solarCurrent: number;
+    regularTotal: number;
+    solarTotal: number;
+  };
+  weapons: Weapon[];
+}
+
+enum WeaponType {
+  mortal = 'MORTAL',
+  artifact = 'ARTIFACT',
+}
+enum WeaponCategory {
+  light,
+  medium,
+  heavy,
+}
+
+export interface Weapon {
+  name: string;
+  type: WeaponType;
+  category: WeaponCategory;
+  accuracy: number;
+  damage: number;
+  defense: number;
+  overwhelming: number;
+  tags: string[];
+}
+
+export enum Attribute {
+  strength = 'STRENGTH',
+  dexterity = 'DEXTERITY',
+  stamina = 'STAMINA',
+  charisma = 'CHARISMA',
+  manipulation = 'MANIPULATION',
+  appearance = 'APPEARANCE',
+  perception = 'PERCEPTION',
+  intelligence = 'INTELLIGENCE',
+  wits = 'WITS',
+}
+
+export enum AttributeCategory {
+  physical = 'PHYSICAL',
+  social = 'SOCIAL',
+  mental = 'MENTAL',
+}
+
+export const ATTRIBUTES = Object.keys(Attribute) as Attribute[];
+export const ATTRIBUTE_GROUPS = {
+  [AttributeCategory.physical]: [Attribute.strength, Attribute.dexterity, Attribute.stamina],
+  [AttributeCategory.social]: [Attribute.charisma, Attribute.manipulation, Attribute.appearance],
+  [AttributeCategory.mental]: [Attribute.perception, Attribute.intelligence, Attribute.wits],
+};
+
+export enum Ability {
+  archery = 'ARCHERY',
+  athletics = 'ATHLETICS',
+  awareness = 'AWARENESS',
+  brawl = 'BRAWL',
+  bureaucracy = 'BUREAUCRACY',
+  craft = 'CRAFT',
+  dodge = 'DODGE',
+  integrity = 'INTEGRITY',
+  investigation = 'INVESTIGATION',
+  larceny = 'LARCENY',
+  linguistics = 'LINGUISTICS',
+  lore = 'LORE',
+  martialArts = 'MARTIAL ARTS',
+  medicine = 'MEDICINE',
+  melee = 'MELEE',
+  occult = 'OCCULT',
+  performance = 'PERFORMANCE',
+  presence = 'PRESENCE',
+  resistance = 'RESISTANCE',
+  ride = 'RIDE',
+  sail = 'SAIL',
+  socialize = 'SOCIALIZE',
+  stealth = 'STEALTH',
+  survival = 'SURVIVAL',
+  thrown = 'THROWN',
+  war = 'WAR',
+}
+
+export const ABILITIES = Object.keys(Ability) as Ability[];
