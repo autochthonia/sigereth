c575e2290151bc18f80cda1c688ce318d7e88d02
Zak Strassberg <zakstrassberg@gmail.com>
2018-08-29 01:15:06 -0400

    Flex

--- src/atoms/Flex.ts
+++ src/atoms/Flex.ts
@@ -1,11 +1,17 @@
 import styled from 'react-emotion';
 
 import basicCss from 'styles/basicCss';
-import flexCss from 'styles/flexCss';
+import flexCss, { FlexCss } from 'styles/flexCss';
 import marginPaddingCss from 'styles/marginPaddingCss';
 import resetCss from 'styles/resetCss';
+import { StyledOtherComponent } from 'create-emotion-styled';
+import { DetailedHTMLProps, HTMLAttributes } from 'react';
 
-const Flex = styled.div(resetCss, flexCss, basicCss, marginPaddingCss);
+const Flex: StyledOtherComponent<
+  FlexCss & { [propName: string]: any },
+  DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement>,
+  any
+> = styled.div(resetCss, flexCss as any, basicCss, marginPaddingCss);
 
 export default Flex;
 export const FlexUl = Flex.withComponent('ul');
