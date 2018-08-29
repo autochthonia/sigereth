e0a68b1f89ba78730d039628d502c17186ee4b8c
Zak Strassberg <zakstrassberg@gmail.com>
2018-08-29 01:14:39 -0400

    flexCss

--- src/styles/flexCss.ts
+++ src/styles/flexCss.ts
@@ -1,3 +1,35 @@
+import {
+  AlignContentProperty,
+  DisplayProperty,
+  AlignItemsProperty,
+  AlignSelfProperty,
+  FlexProperty,
+  FlexBasisProperty,
+  FlexDirectionProperty,
+  FlexFlowProperty,
+  GlobalsNumber,
+  FlexWrapProperty,
+  JustifyContentProperty,
+  PlaceContentProperty,
+} from 'csstype';
+
+export interface FlexCss {
+  display?: DisplayProperty;
+  alignContent?: AlignContentProperty;
+  alignItems?: AlignItemsProperty;
+  alignSelf?: AlignSelfProperty;
+  flex?: FlexProperty<string | 0>;
+  flexBasis?: FlexBasisProperty<string | 0>;
+  flexDirection?: FlexDirectionProperty;
+  flexFlow?: FlexFlowProperty;
+  flexGrow?: GlobalsNumber | GlobalsNumber[];
+  flexShrink?: GlobalsNumber | GlobalsNumber[];
+  flexWrap?: FlexWrapProperty;
+  justifyContent?: JustifyContentProperty;
+  order?: GlobalsNumber;
+  placeContent?: PlaceContentProperty;
+}
+
 export default ({
   display = 'flex',
   alignContent,
@@ -13,9 +45,7 @@ export default ({
   justifyContent,
   order,
   placeContent,
-}: {
-  [cssProperty: string]: any;
-}) => ({
+}: FlexCss) => ({
   display,
   alignContent,
   alignItems,
