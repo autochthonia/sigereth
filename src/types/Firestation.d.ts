a1edcf3934be0329451fac18595663f58ef32c9d
Zak Strassberg <zakstrassberg@gmail.com>
2018-08-30 11:27:06 -0400

    new types

--- src/types/Firestation.d.ts
+++ src/types/Firestation.d.ts
@@ -1,6 +1,19 @@
 import firebase from 'firebase';
 
-export interface Document<DataType> {
+export interface DocumentSnapshot<DataType = DocumentData>
+  extends firebase.firestore.DocumentSnapshot {
+  data(options?: SnapshotOptions): DataType | undefined;
+}
+export interface QueryDocumentSnapshot<DataType = DocumentData>
+  extends firebase.firestore.QueryDocumentSnapshot {
+  data(options?: SnapshotOptions): DataType | undefined;
+}
+
+export interface QuerySnapshot<DataType = DocumentData> extends firebase.firestore.QuerySnapshot {
+  readonly docs: QueryDocumentSnapshot<DataType>[];
+}
+
+export interface DocumentSnapshotExpanded<DataType = DocumentData> {
   exists: firebase.firestore.DocumentSnapshot['exists'];
   ref: firebase.firestore.DocumentSnapshot['ref'];
   id: firebase.firestore.DocumentSnapshot['id'];
@@ -8,11 +21,11 @@ export interface Document<DataType> {
   data: DataType;
 }
 
-export interface Query<DataType> {
+export interface QuerySnapshotExpanded<DataType = DocumentData> {
   metadata: firebase.firestore.QuerySnapshot['metadata'];
   size: firebase.firestore.QuerySnapshot['size'];
   empty: firebase.firestore.QuerySnapshot['empty'];
   docs: {
-    [docId: string]: Document<DataType>;
+    [docId: string]: DocumentSnapshotExpanded<DataType>;
   };
 }
