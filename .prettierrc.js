3b6f9bcb6fd68588d72aa99097ea88f0fe4f9cd0
Zak Strassberg <zakstrassberg@gmail.com>
2018-09-29 16:39:41 -0400

    jest

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   useTabs: false, // Indent lines with tabs instead of spaces.
-  printWidth: 120, // Specify the length of line that the printer will wrap on.
+  printWidth: 100, // Specify the length of line that the printer will wrap on.
   tabWidth: 2, // Specify the number of spaces per indentation-level.
   singleQuote: true, // Use single quotes instead of double quotes.
   /**
--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,4 @@
 module.exports = {
-  roots: ['<rootDir>/src'],
-  transform: {
-    '^.+\\.(t|j)sx?$': 'ts-jest',
-  },
-  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  globals: {
-    'ts-jest': {
-      useBabelrc: true,
-    },
-  },
+  preset: 'ts-jest',
+  testEnvironment: 'node',
 };
