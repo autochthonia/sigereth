84d359534c16b1dbfc027c079cd92aa93ae066cc
Zak Strassberg <zakstrassberg@gmail.com>
2018-09-30 21:52:28 -0400

    GamePage

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -1,60 +1,79 @@
-import React, { SFC } from 'react';
+import React, { Component } from 'react';
 import Flex from 'atoms/Flex';
 import CombatantList from 'organisms/CombatantList';
-import { Game, Message } from 'types/Game';
+import { Game as IGame, Message, Player } from 'types/Game';
 import { Combat, Combatant } from 'types/Combat';
-import { DocumentSnapshotExpanded, CollectionReference } from 'types/Firestation';
+import {
+  DocumentSnapshotExpanded,
+  CollectionReference,
+  QuerySnapshotExpanded,
+} from 'types/Firestation';
 import MessagesContainer from 'containers/Game/Messages';
+import AddCombatant from 'organisms/AddCombatant';
+import { PMessagesOrganism } from 'organisms/Messages';
 import { firestore } from 'firebase';
 import { getUID } from 'services/firestation';
 
 interface GameHeaderProps {
-  name: Game['name'];
+  name: IGame['name'];
 }
 const Header = Flex.withComponent('header');
 const GameHeader = ({ name }: GameHeaderProps) => <Header>{name}</Header>;
 
 interface CombatInfoProps {
   turn: Combat['turn'];
-  activeCombatant: Combatant;
+  activeCombatant: DocumentSnapshotExpanded<Combatant>;
 }
 const Article = Flex.withComponent('section');
 const CombatInfo = ({ turn, activeCombatant }: CombatInfoProps) => (
   <Article>
-    Turn {turn} - Active Combatant: {activeCombatant ? activeCombatant.name : 'None'}
+    Turn {turn} - Active Combatant: {activeCombatant ? activeCombatant.data.name : 'None'}
   </Article>
 );
 
-type GameProps = {
-  game: DocumentSnapshotExpanded<Game>;
+interface PGame {
+  game: DocumentSnapshotExpanded<IGame>;
   combat: DocumentSnapshotExpanded<Combat>;
-  orderedCombatants: Combatant[];
-  activeCombatant: Combatant;
-};
+  activeCombatant: DocumentSnapshotExpanded<Combatant>;
+  orderedCombatants: DocumentSnapshotExpanded<Combatant>[];
+  players: QuerySnapshotExpanded<Player>;
+}
 
-const Game: SFC<GameProps> = ({ orderedCombatants, activeCombatant, game, combat }) => (
-  <Flex flexDirection="column">
-    <GameHeader name={game.data.name} />
-    <Flex>
+class Game extends Component<PGame> {
+  messagesRef: CollectionReference<Message> = this.props.game.ref.collection(
+    'messages',
+  ) as CollectionReference<Message>;
+  sendMessage: PMessagesOrganism['sendMessage'] = async m =>
+    this.props.game.ref.collection('messages').add({
+      // TODO: sanitize message
+      body: m,
+      createdAt: firestore.FieldValue.serverTimestamp(),
+      sender: firestore().doc(`users/${getUID()}`),
+    } as Message);
+  render() {
+    const { orderedCombatants, activeCombatant, game, combat, players } = this.props;
+    return (
       <Flex flexDirection="column">
-        <CombatInfo activeCombatant={activeCombatant} turn={combat.data.turn} />
-        <CombatantList combatants={orderedCombatants} activeCombatant={activeCombatant} />
+        <GameHeader name={game.data.name} />
+        <Flex>
+          <Flex flexDirection="column">
+            <CombatInfo activeCombatant={activeCombatant} turn={combat.data.turn} />
+            <CombatantList combatants={orderedCombatants} activeCombatant={activeCombatant} />
+            <AddCombatant
+              addCombatant={combatant => combat.ref.collection('combatants').add(combatant)}
+            />
+          </Flex>
+          <Flex>
+            <MessagesContainer
+              messages={this.messagesRef}
+              sendMessage={this.sendMessage}
+              players={players}
+            />
+          </Flex>
+        </Flex>
       </Flex>
-      <Flex>
-        <MessagesContainer
-          messages={game.ref.collection('messages') as CollectionReference<Message>}
-          sendMessage={async m =>
-            game.ref.collection('messages').add({
-              // TODO: sanitize message
-              body: m,
-              createdAt: firestore.FieldValue.serverTimestamp(),
-              sender: firestore().doc(`users/${getUID()}`),
-            } as Message)
-          }
-        />
-      </Flex>
-    </Flex>
-  </Flex>
-);
+    );
+  }
+}
 
 export default Game;
