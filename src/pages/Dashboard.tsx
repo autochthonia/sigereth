d5e39fee8a8c9709b8143c43a4f4c4268f332772
Zak Strassberg <zakstrassberg@gmail.com>
2018-09-30 21:52:16 -0400

    Dashboard

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,11 +1,24 @@
 import React, { SFC } from 'react';
 import Flex from 'atoms/Flex';
+import { map } from 'lodash';
 import { Link } from 'found';
+import { QuerySnapshotExpanded } from 'types/Firestation';
+import { Game } from 'types/Game';
 
-const DashboardView: SFC = () => (
-  <Flex>
-    <Link to="/games/malfeas">Malfeas</Link>
+interface PDashboardPage {
+  games: QuerySnapshotExpanded<Game>;
+}
+
+const DashboardPage: SFC<PDashboardPage> = ({ games }) => (
+  <Flex flexDirection="column">
+    <p>Debug mode - all games shown</p>
+    {map(games.docs, g => (
+      <Link key={g.id} to={`/games/${g.id}`}>
+        {g.data.name}
+      </Link>
+    ))}
+    <Link to="/games/new">Create game</Link>
   </Flex>
 );
 
-export default DashboardView;
+export default DashboardPage;
