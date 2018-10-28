ec117e7b9ad473f0f7d903dd7e52a6e84f9d10ae
Zak Strassberg <zakstrassberg@gmail.com>
2018-10-28 16:01:41 -0400

    createFSUserRef default

--- src/services/fsSelector.ts
+++ src/services/fsSelector.ts
@@ -3,9 +3,11 @@ import { User } from 'types/User';
 import { Game } from 'types/Game';
 // @ts-ignore
 import { useMemo } from 'react';
+import { getUID } from './firestation';
 
-export const createFSUserRef: (userId: string) => firestore.DocumentReference<User> = userId =>
-  firestore().doc<User>(`users/${userId}`);
+export const createFSUserRef: (userId?: string) => firestore.DocumentReference<User> = (
+  userId = getUID(),
+) => firestore().doc<User>(`users/${userId}`);
 
 export const createFSGameRef: (gameId: string) => firestore.DocumentReference<Game> = gameId =>
   useMemo(
